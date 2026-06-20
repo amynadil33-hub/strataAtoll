ebc45e8dd5919f3a4d5058eeecd8022df67a1cf0
amynadil33-hub <amynadil33@gmail.com>
2026-06-21 00:11:14 +0500

    changes

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,18 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-import "./.next/dev/types/routes.d.ts";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "izyihjihnuygjiikeirp.supabase.co",
+        pathname: "/storage/v1/object/**",
+      },
+    ],
+  },
+}
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+export default nextConfig
